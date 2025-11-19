4d3aad7141a6cd6b2a633c02ec50124ec2174f88
GraceLaMar <lamarg@huntington.edu>
2025-11-19 16:05:29 -0500

    Update create-tables.sql

--- create-tables.sql
+++ create-tables.sql
@@ -98,7 +98,7 @@ VALUES
 (1, 'Landon', 'Biegel', 15, 'F', 'SO', 77, 180),
 (1, 'Lane', 'Sparks', 20, 'G', 'SR', 75, 185),
 (1, 'Landen', 'Jordan', 23, 'F', 'SR', 81, 210),
-(1, 'Jackson', 'Paul', 24, 'G', SR', 73, 175),
+(1, 'Jackson', 'Paul', 24, 'G', 'SR', 73, 175),
 (1, 'Luke', 'LaGrange', 41, 'F', 'JR', 77, 195),
     
 (2, 'DJ', 'Moore', 0, 'F', 'JR', 80, 190),
@@ -117,9 +117,9 @@ VALUES
 (2, 'Cliff', 'Gashumba', 33, 'F', 'SO', 78, 200),
 (2, 'Caedmon', 'Bontrager', 35, 'F', 'SO', 79, 235),
     
-(3, 'Payton', 'Baird', 0, 'G', 'FR', 77, 235),
-(3, 'Jameir', 'Jefferson', 1, 'G', 'SO', 74, 235),
-(3, 'Colin', 'Comer', 3, 'G', 'RS', 76, 235),
+(3, 'Payton', 'Baird', 0, 'G', 'FR', 77, 185),
+(3, 'Jameir', 'Jefferson', 1, 'G', 'SO', 74, 180),
+(3, 'Colin', 'Comer', 3, 'G', 'RS', 76, 185),
 (3, 'Beau', 'Jacquay', 4, 'C', 'SO', 80, 235),
 (3, 'Austin', 'Cripe', 5, 'G', 'RS', 75, 235),
 (3, 'Andrew', 'Hedrick', 10, 'G', 'SO', 72, 235),
@@ -196,8 +196,8 @@ VALUES
 
 (8, 'Fletcher', 'Manhart', 0, 'G', 'SR', 71, 190),
 (8, 'David', 'Cundiff', 1, 'G', 'FR', 72, 170),
-(8, 'Travis', 'Grayson', 2, 'G', 'JR', 75, NULL),
-(8, 'Shane', 'Rykse', 'G', 3, 'FR', 75, 190),
+(8, 'Travis', 'Grayson', 2, 'G', 'JR', 75, 195),
+(8, 'Shane', 'Rykse', 3, 'G', 'FR', 75, 190),
 (8, 'Tyler', 'DeSimpelare', 4, 'G', 'JR', 75, 165),
 (8, 'Warrick', 'Elder', 5, 'G', 'FR', 73, 165),
 (8, 'Trey', 'Cottingham', 10, 'G', 'SR', 73, 180),
